146bf684ed0e2fea47841bdf644ee1914f23ee69
Kate Anderson <90657569+kanderson250@users.noreply.github.com>
2022-06-15 20:01:29 -0700

    Time: 98 ms (33.51%), Space: 44.1 MB (32.04%) - LeetHub

--- 133-clone-graph/133-clone-graph.js
+++ 133-clone-graph/133-clone-graph.js
@@ -16,21 +16,36 @@ var cloneGraph = function(node) {
         return null; 
     }
     const visited = {}; //contains numeric keys whose values are references to COPIED graph nodes. 
-    const queue = []; //a queue representing nodes left to be traversed in the ORIGINAL graph. Only nodes of the ORIGINAL should be added to this. 
-    visited[node.val] = new Node(node.val); 
-    queue.push(node);
-    while (queue.length) {
-        current = queue.shift();
-        currentCopy = visited[current.val];
-        for (let neighbor of current.neighbors) {
-            if (!(neighbor.val in visited)) {
-                //make a copy in the visited object
-                visited[neighbor.val] = new Node(neighbor.val);
-                //add the node itself to the queue traversing the original graph. 
-                queue.push(neighbor);
+    const stack = []; //contains references to the ORIGINAL graph, for a DFS. 
+    visited[node.val] = new Node(node.val);
+    stack.push(node);
+    //perform DFS. 
+    while (stack.length) {
+        let v = stack.pop();
+        for (let u of v.neighbors) { 
+            if (!visited[u.val]) {
+              stack.push(u);  
+              visited[u.val] = new Node(u.val);
             }
-            currentCopy.neighbors.push(visited[neighbor.val]);
+            visited[v.val].neighbors.push(visited[u.val]);
         }
-    }
+    }   
+       
+    // const queue = []; //a queue representing nodes left to be traversed in the ORIGINAL graph. Only nodes of the ORIGINAL should be added to this. 
+    // visited[node.val] = new Node(node.val); 
+    // queue.push(node);
+    // while (queue.length) {
+    //     current = queue.shift();
+    //     currentCopy = visited[current.val];
+    //     for (let neighbor of current.neighbors) {
+    //         if (!(neighbor.val in visited)) {
+    //             //make a copy in the visited object
+    //             visited[neighbor.val] = new Node(neighbor.val);
+    //             //add the node itself to the queue traversing the original graph. 
+    //             queue.push(neighbor);
+    //         }
+    //         currentCopy.neighbors.push(visited[neighbor.val]);
+    //     }
+    // }
     return visited[node.val];
 };
