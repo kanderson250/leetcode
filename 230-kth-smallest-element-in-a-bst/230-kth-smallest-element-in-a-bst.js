d6faf334de8e00192a5e4383cb4ea655cf4acf43
Kate Anderson <90657569+kanderson250@users.noreply.github.com>
2022-06-29 11:12:19 -0700

    Time: 113 ms (42.92%), Space: 48.2 MB (73.87%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.js
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.js
@@ -14,17 +14,39 @@
 var kthSmallest = function(root, k) {
     //do a depth first inorder traversal
     //return the kth non-null node. 
-    let ordering = [];     
+    //slow because its runtime is O(n + k) = O(n). 
+    //The runtime should be O(k). 
+    
+//     let ordering = [];     
+    
+//     function inOrder(node) {
+//         if (node) {
+//             inOrder(node.left);
+//             ordering.push(node.val);
+//             inOrder(node.right);
+//         }
+//     }
+    
+//     inOrder(root);
+//     return ordering[k-1];
+    
+    let counter = 0; 
     
     function inOrder(node) {
         if (node) {
-            inOrder(node.left);
-            ordering.push(node.val);
-            inOrder(node.right);
+            let left = inOrder(node.left);
+            if (left !== null) {
+                return left; 
+            }
+            counter++; 
+            if (counter === k) {
+                return node.val; 
+            }
+            return inOrder(node.right);
         }
+        return null;
     }
     
-    inOrder(root);
-    return ordering[k-1];
+    return inOrder(root);
     
 };
