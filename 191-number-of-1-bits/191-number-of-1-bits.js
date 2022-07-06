39f206a74f03480244250ccce71472050f3c21ab
Kate Anderson <90657569+kanderson250@users.noreply.github.com>
2022-07-06 12:21:02 -0700

    Time: 125 ms (13.27%), Space: 42.4 MB (62.36%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.js
+++ 191-number-of-1-bits/191-number-of-1-bits.js
@@ -5,13 +5,20 @@
 var hammingWeight = function(n) {
     //n is always a binary string of length 32
     //I would call it until the string is empty. 
+    // let ones = 0; 
+    // let m = 1;  
+    // for (let i = 0; i < 32; i++) {
+    //     if ((m & n) !== 0) {
+    //         ones++; 
+    //     }
+    //     m<<=1; 
+    // }
+    // return ones; 
+    
     let ones = 0; 
-    let m = 1;  
-    for (let i = 0; i < 32; i++) {
-        if ((m & n) !== 0) {
-            ones++; 
-        }
-        m<<=1; 
+    while (n!==0) {
+        n = n & (n-1);
+        ones++; 
     }
     return ones; 
 };
