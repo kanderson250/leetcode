e411583854f158d477fed2d4aba5d5626a57b4aa
Kate Anderson <90657569+kanderson250@users.noreply.github.com>
2022-07-08 10:46:55 -0700

    Time: 111 ms (62.00%), Space: 46.7 MB (45.84%) - LeetHub

---
+++ 55-jump-game/55-jump-game.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    //create a results array m to store the DP model
+    //const m = Array(nums.length).fill(false);
+    
+    //IMPROVED SOLUTION --- O(n)
+    let lastGood = nums.length - 1;
+    for (let i = nums.length - 2; i >= 0; i--){
+        //check if lastGood index is within jumping reach of i
+        if (lastGood <= i + nums[i]) {
+            lastGood = i; 
+        }
+    }
+    return lastGood === 0; 
+    
+    
+    //ORIGINAL SOLUTION, O(n^2)
+    //set position nums.length -1 to be true - the last index can always reach itself!
+//     nums[nums.length - 1] = true; 
+//     //iterate backwards through m, starting in position nums.length - 2
+//     for (let i = nums.length - 2; i >= 0; i--) {
+//         let jump = nums[i];
+//         nums[i] = false; 
+//         for (let j = 1; j <= jump; j++) {
+//             if (nums[i+j]) {
+//                 nums[i] = true; 
+//                 break; 
+//             }
+//         }
+//     }
+    
+//     return nums[0]; 
+};
