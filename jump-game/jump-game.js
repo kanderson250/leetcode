4be0dea59ac4f3ff47e3117b76fabfc9ed53acd1
Kate Anderson <90657569+kanderson250@users.noreply.github.com>
2022-07-08 10:39:49 -0700

    Time: 444 ms, Memory: 46.6 MB - LeetHub

--- jump-game/jump-game.js
+++ jump-game/jump-game.js
@@ -4,18 +4,20 @@
  */
 var canJump = function(nums) {
     //create a results array m to store the DP model
-    const m = Array(nums.length).fill(false);
+    //const m = Array(nums.length).fill(false);
     //set position nums.length -1 to be true - the last index can always reach itself!
-    m[nums.length - 1] = true; 
+    nums[nums.length - 1] = true; 
     //iterate backwards through m, starting in position nums.length - 2
     for (let i = nums.length - 2; i >= 0; i--) {
-        for (let j = 1; j <= nums[i]; j++) {
-            if (m[i+j]) {
-                m[i] = true; 
+        let jump = nums[i];
+        nums[i] = false; 
+        for (let j = 1; j <= jump; j++) {
+            if (nums[i+j]) {
+                nums[i] = true; 
                 break; 
             }
         }
     }
     
-    return m[0]; 
+    return nums[0]; 
 };
