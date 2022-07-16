c65110a257d1256b93d916fc58183887e34bb7f2
Kate Anderson <90657569+kanderson250@users.noreply.github.com>
2022-07-16 14:25:44 -0700

    Time: 71 ms (80.01%), Space: 41.8 MB (72.09%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.js
+++ 28-implement-strstr/28-implement-strstr.js
@@ -3,22 +3,16 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-    if (!needle.length) {
-        return 0; 
-    }
-    //naive solution
-    //iterate over haystack, iterate over needle
-    //return index in haystack when needle is first found. 
+var strStr = function(haystack, needle) { 
     for (let i = 0; i <= haystack.length - needle.length; i++) {
-        let found = true; 
         for (let j = 0; j < needle.length; j++) {
-            if (needle[j] !== haystack[i + j]) {
-                found = false; 
+            if (haystack[i + j] !== needle[j]) {
                 break; 
             }
+            if (j === needle.length - 1) {
+                return i; 
+            }
         }
-        if (found) return i; 
     }
     return -1; 
 };
