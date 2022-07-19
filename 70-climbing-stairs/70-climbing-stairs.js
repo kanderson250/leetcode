ae10bd7d4f901b67f12abe94c56aa26a71b97eb3
Kate Anderson <90657569+kanderson250@users.noreply.github.com>
2022-07-18 17:18:12 -0700

    Time: 67 ms (82.60%), Space: 42 MB (43.02%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -3,10 +3,17 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    let a = 1; 
-    let b = 2;
-    for (let i = 1; i < n; i++) {
-        [b, a] = [a+b, b];
+    //input: integer (number of stairs)
+    //output: integer (number of valid combinations)
+    //constraints: only 1 or 2 steps at a time; n >= 1. 
+    //edge cases: none. 
+    
+    //notice: ways(n) = ways(n-2) + ways(n-1).
+    //thus, this is a one dimensional dynamic programming question
+    let a = 0; 
+    let b = 1; 
+    for (let i = 1; i <= n; i++) {
+        [a, b] = [b, a + b];
     }
-    return a;    
+    return b; 
 };
